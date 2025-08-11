70270697afaa7601aaf7693ef18069de42937681
tienlm <tienlm@vtc.vn>
2025-08-11 10:25:33 +0700

    update link

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
+export async function getServerSideProps(context) {
+  const { link } = context.query;
+
+  if (link) {
+    return {
+      redirect: {
+        destination: link,
+        permanent: false, // false = 302, true = 301
+      },
+    };
+  }
+
+  return {
+    props: {}, // Nếu không có link thì hiển thị trang mặc định
+  };
+}
+
 export default function Home() {
-  return (
-    <div>
-      <h1>Redirect API Home</h1>
-      <p>Welcome to Redirect API built with Next.js!</p>
-    </div>
-  );
+  return <h1>Vui lòng truyền ?link=URL để redirect</h1>;
 }
